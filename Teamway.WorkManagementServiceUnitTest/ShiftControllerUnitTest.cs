14062754532f2465bfb974fcd5e49d4756de53f1
Dariusz Pelka <dariusz.pelka@aerdata.com>
2021-01-24 15:24:33 +0100

    Add unit test checking adding of shifts

--- Teamway.WorkManagementServiceUnitTest/ShiftControllerUnitTest.cs
+++ Teamway.WorkManagementServiceUnitTest/ShiftControllerUnitTest.cs
@@ -40,6 +40,36 @@ namespace Teamway.WorkManagementServiceUnitTest
             Assert.AreEqual(1, okResult.Value);
         }
 
+
+        [Test]
+        [TestCase(2020, 1, 2, ShiftType.ShiftFrom0To8, 1)]
+        [TestCase(2020, 1, 3, ShiftType.ShiftFrom16To24, 1)]
+        [TestCase(2020, 1, 1, ShiftType.ShiftFrom0To8, 1)]
+        public void Add_WhenShiftSeparatedFromExisting_ThenShiftShouldBeAdded(int year, int month, int day, ShiftType type, int workerId)
+        {
+            // Arrange
+            var shift = new Shift() { Day = new DateTime(year, month, day), Type = type, WorkerId = workerId };
+            var list = new List<Shift>();
+            list.Add(shift);
+            var mockedRepository = new Mock<IRepository>();
+            mockedRepository.Setup(m => m.AddShift(It.IsAny<AddShift>())).Returns(1);
+            mockedRepository.Setup(m => m.GetWorker(It.IsAny<int>())).Returns(new Worker() { Id = 1, FirstName = "Jan", LastName = "Hello" });
+            mockedRepository.Setup(m => m.GetShiftsPerWorker(It.IsAny<int>())).Returns(new List<Shift>());
+
+            var controller = new ShiftController(mockedRepository.Object);
+            var newShift = new AddShift() { Day = new DateTime(2020, 1, 2), Type = ShiftType.ShiftFrom16To24, WorkerId = 1 };
+            // Act
+
+            var result = controller.Add(newShift);
+            var okResult = result as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(1, okResult.Value);
+        }
+
+        // Test checking if shift can be added having same time as one we try to add or if we have a shift which is before or after one we try to add
         [Test]
         [TestCase(2020, 1, 2, ShiftType.ShiftFrom0To8, 1)]
         [TestCase(2020, 1, 2, ShiftType.ShiftFrom16To24, 1)]
@@ -57,20 +87,19 @@ namespace Teamway.WorkManagementServiceUnitTest
 
             var controller = new ShiftController(mockedRepository.Object);
             var newShift = new AddShift() { Day = new DateTime(2020, 1, 1), Type = ShiftType.ShiftFrom0To8, WorkerId = 1 };
-            // Act
 
+            // Act
             try
             {
-                var result = controller.Add(newShift);
+                controller.Add(newShift);
             }
             catch (HttpResponseException exception)
             {
+                // Assert
                 Assert.IsTrue(true);
                 var errorMessage = exception.Response.Content.ReadAsStringAsync().Result;
                 Assert.AreEqual("Shift same, previous ot next exists", errorMessage);
             }
-            
-            // Assert
         }
     }
 }
